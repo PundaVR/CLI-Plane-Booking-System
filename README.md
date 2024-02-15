fc536b822fdb73e2c185704927c579f24d4380f0
Tal Carmel <talcarmel787@gmail.com>
2024-02-16 10:45:32 +1100

    need to do seat balancing system and unit tests then gg done

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # CLI-Plane-Booking-System
 A plane booking system in the command line, highly customisable. Written in Python. School work.
 Assignment is a create a plane booking system with columns ABC & DEF with 31 lines of seats.
+
+# Requirements:
+- Program loops until prompted to exit
+- Passenger and Staff menus
+- Seats B1, B2, B3, E1, E2, and E3 - Cannot be booked and must display as "unvavailable"
+
+## Passengers 
+Passengers must be booked one at a time
+- Passenger portal loops until prompted to exit to main menu
+- Ask for passenger full name and seat choice (seat booking formatted like B7, D29, etc)
+- If a seat is already booked, display "Seat unavailable" and let the user try again
+- Seat allocation system will distrubite weight across the plane. Columns [A B C] are grouped as one side, and [D E F] as the other. If one side has 10 more booked seats than the other, it will lock passengers from booking that side until balance has been restored (less than 10 seats more) 
+
+## Staff
+- Enter a seat (i.e. F4) and be able to view the name of the person in there
--- main.py
+++ main.py
@@ -1,14 +1,58 @@
+AppConfigs = {
+    "debug": True,
+    "requirePromptDisplaySeats": False
+}
+
+manifest = {} # seat : passenger 
+
 numberOfSeats = 31
 
 numbersPort = numberOfSeats
 charactersPort = 3
-seatsPort = [['[ ]' for i in range(numbersPort)] for j in range(charactersPort)]
+
+seatBooked = '[X]'
+seatAvailable = '[0]'
+
+seatsPort = [[seatAvailable for i in range(numbersPort)] for j in range(charactersPort)]
 
 numbersStarboard = numberOfSeats
 charactersStarboard = 3
-seatsStarboard = [['[ ]' for i in range(numbersStarboard)] for j in range(charactersStarboard)]
+seatsStarboard = [[seatAvailable for i in range(numbersStarboard)] for j in range(charactersStarboard)]
+
+
 
 # LEVEL 3 FUNCTIONS - ERROR CHECKING + HELPERS OF HELPERS
+
+def ValidateUserInputNumbers(userInput, maxMenuNumberOption, excludeZero = False):
+    validInput = False
+    if (userInput.isnumeric() and int(userInput) <= maxMenuNumberOption and int(userInput) >= 0):
+        if(excludeZero and userInput=="0"):
+            validInput = False
+        else:
+            validInput = True
+    return validInput
+
+def ValidateSeatInput(seat=""):
+    isValid = False
+    if(len(seat) == 2):
+        if (seat[0].isalpha() and seat[1].isnumeric()): # check string is 2 characters (1st is num and 2nd is char)
+            if (ord(seat[0]) >= 65 and ord(seat[0]) <= (64+charactersPort+charactersStarboard) and int(seat[1]) > 0 and int(seat[1]) <= numberOfSeats):
+                isValid = True
+    elif(len(seat) == 3):
+        if (seat[0].isalpha() and seat[1].isnumeric() and seat[2].isnumeric()): # check string is 2 characters (1st is num and 2nd is char)
+            if (ord(seat[0]) >= 65 and ord(seat[0]) <= (64+charactersPort+charactersStarboard) and int(seat.split(seat[0])[-1]) > 0 and int(seat.split(seat[0])[-1]) <= numberOfSeats):
+                isValid = True
+
+    return isValid
+
+def InputSeat():
+    seat = "null"
+    seatInput = input("Choose Seat (i.e. B7):\n>>>").upper()
+    while(ValidateSeatInput(seatInput) == False):
+        seatInput = input("Choose Seat (i.e. B7):\n>>>").upper()
+    seat = seatInput
+    return seat
+
 
 # LEVEL 2 FUCNTIONS - HELPERS
 
@@ -17,21 +61,75 @@ def SeatConversion(c, n):
     newNum = n + 1
     return [newChar, newNum]
 
-def ConvertCharacterToNum(c="A", portSide = False):
+def ConvertCharacterToNum(c="A"):
     changedChar = ord(c.upper())
     convertedNum = 0
+    isPortSide = False
     if changedChar < 65+charactersPort: #or portSide
         convertedNum = charactersPort-(changedChar-65)
+        isPortSide =  True
     else:
-        convertedNum = charactersStarboard-(changedChar-(65+charactersPort))
+        convertedNum = charactersStarboard-(changedChar-(65+charactersPort)-1)
 
-    return convertedNum
+    return (convertedNum, isPortSide)
 
+def StoreBookingInformation(seat, passenger):
+    manifest[seat]= passenger
+    # store to a file as well
+    print(manifest)
+
+def SeatGet(rowStr: str, seatNumStr: str):
+    isAvailable = True
+    if (seatNumStr.isnumeric()):
+        seatNum = int(seatNumStr)-1
+        row = ConvertCharacterToNum(rowStr)[0]
+        portSide = ConvertCharacterToNum(rowStr)[1]
+
+        if portSide:
+            if (seatsPort[row-1][seatNum] == seatBooked):
+                isAvailable = False
+        elif (seatsStarboard[row-charactersPort-2][seatNum] == seatBooked):
+           isAvailable = False
+        else: isAvailable = True
+    return isAvailable
+
+def SeatSet(rowStr, seatNumStr):
+    success = False
+    if (seatNumStr.isnumeric() and SeatGet(rowStr, seatNumStr)):
+        seatNum = int(seatNumStr)-1
+        row = ConvertCharacterToNum(rowStr)[0]
+        portSide = ConvertCharacterToNum(rowStr)[1]
+        
+        if portSide:
+            seatsPort[row-1][seatNum] = seatBooked
+            success = True
+        else:
+            seatsStarboard[row-charactersPort-2][seatNum] = seatBooked
+            success = True
+    else:
+        print("Seat unavailable")
+    return success
+
+def SeatLookup(seat: str):
     
+    seatInfo = manifest.get(seat)
+    if (seatInfo is None):
+        print("Seat Available")
+    else:
+        print(seatInfo)
+        
 
 # LEVEL 1 FUNCTIONS - USED IN MAIN CODE
 
-def DisplaySeats():
+def DisplaySeats(display = False):
+    # Check if user wants to display the seat layout
+    if (display == False and AppConfigs["requirePromptDisplaySeats"]):
+        inp = input("Display seating diagram? y/n\n")
+        print("\n")
+        if(inp.lower() != "y"):
+            return
+    
+    # Display Seat Numbers
     seatNumbers = "  "
     for i in range(1, numberOfSeats+1):
         if i == 10:
@@ -43,7 +141,7 @@ def DisplaySeats():
         
     print(seatNumbers)
 
-
+    # Display Starboard (Right) Seats
     letter = 64 + charactersPort + charactersStarboard# chr(65) = A
 
     for x in seatsStarboard:
@@ -53,12 +151,14 @@ def DisplaySeats():
         
         print(f"{chr(letter)} {lettersStarboard}")
         letter-=1
-        
+
+    # Display Middle Walkway 
     walkway = "  "
     for a in range(numberOfSeats):
         walkway+="==="
     print(walkway)
 
+    # Display Port (Left) Seats
     for x in seatsPort:
         lettersPort = ""
         for y in x:
@@ -67,16 +167,99 @@ def DisplaySeats():
         print(f"{chr(letter)} {lettersPort}")
         letter-=1
     
-def BookSeat(c, n):
-    seatNum = n-1
-    if c > charactersPort:
-        seatsStarboard[c-charactersPort-2][seatNum] = "[X]"
-    else:
-        seatsPort[c-1][seatNum] = "[X]"
+def BookSeat(rowStr: str, seatNumStr: str, passengerFullName = "Seat Unavailable"):
+    success = SeatSet(rowStr, seatNumStr)
+
+    if success == True:
+        StoreBookingInformation(f"{rowStr}{seatNumStr}", passengerFullName)
+    return success
+
+def BlockSeats():
+    BookSeat("B", "1") # B1
+    BookSeat("B", "2") # B2
+    BookSeat("B", "3") # B3
+    BookSeat("E", "1") # E1
+    BookSeat("E", "2") # E2
+    BookSeat("E", "3") # E3
 
 
-# A to C WORKS, NEED TO IMPLEMENT CONVERTCHAR IN BookSeat() AND ADD LOGIC TO DETERMINE IF ITS PORT OR STARBOARD
-x = ConvertCharacterToNum("A") #A=0 B=1 C=2 --  
-print(x)
-BookSeat(x,2)
-DisplaySeats()
+def DisplayMenu():
+    # display menu options
+    print("\n\n1. Passenger Portal")
+    print("2. Staff Portal")
+    print("0. Exit Program")
+    inp = input("Select an option: ")
+    while (ValidateUserInputNumbers(inp, 2) is False):
+        inp = input("Invalid Input!\nSelect an option: ")
+    match (inp):
+        case "0":
+            quit()
+        case "1":
+            PassengerPortal()
+        case "2": 
+            StaffPortal()
+
+
+def StaffPortal():
+    # display menu options
+    print("1. Seat Lookup")
+    print("0. Return to Menu")
+    inp = input("Select an option: ")
+    while (ValidateUserInputNumbers(inp, 1) is False):
+        inp = input("Invalid Input!\nSelect an option: ")
+    match (inp):
+        case "0":
+            DisplayMenu()
+        case "1":
+            DisplaySeats(True)
+            seat = InputSeat()
+            SeatLookup(seat)
+
+
+def PassengerPortal():
+    DisplaySeats()
+    passengerFullName = input("Enter full name:\n")
+    
+    passengerSeat = InputSeat()
+
+    while(BookSeat(passengerSeat[0], passengerSeat.split(passengerSeat[0])[-1], passengerFullName) is False):
+        passengerSeat = InputSeat()
+    print("Booked Seat!")
+
+
+
+def BookingSystem():
+    # clear database etc etc
+    BlockSeats()
+    while(True):
+        DisplayMenu()
+
+def UnitTest_UserInputs():
+    print("TESTING FUNCTION: ValidateUserInputNumbers()")
+    for i in range(-1, 10, 1):
+        print(f"{i} -> (maxNumber = 5, excludeZero = False): {ValidateUserInputNumbers(str(i), 5)}")
+        print(f"{i} -> (maxNumber = 2, excludeZero = True): {ValidateUserInputNumbers(str(i), 2, True)}")
+
+def UnitTest_SeatInputs():
+    print("TESTING FUNCTION: ValidateSeatInput()")
+    for i in range(-2, 10):
+        print(f"{chr(i+64)}{3*i} : {ValidateSeatInput(f'{chr(i+65)}{3*i}')}")
+        print(f"{chr(i+80)}{3+(i*2)} : {ValidateSeatInput(f'{chr(i+80)}{3+(i*2)}')}")
+        
+def UnitTest_BookSeat():
+    pass
+
+def UnitTest_SeatSet():
+    pass
+
+def UnitTest_SeatGet():
+    pass
+
+def UnitTest_SeatLookup():
+    pass
+
+#UnitTest_UserInputs()
+#UnitTest_SeatInputs()
+#
+BookingSystem()
+
